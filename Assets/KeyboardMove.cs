641da3ece51ad48c041c2ef7cb2a49fcd5cb7e36
Andrey Morozov <morozov@client-line.com>
2015-12-06 21:30:14 +0200

    Added keyboard moving and gravity

---
+++ Assets/KeyboardMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/KeyboardMove")]
+public class KeyboardMove : MonoBehaviour {
+
+    public float Speed = 4f;
+    public float Gravity = -9.8f;
+
+    private CharacterController _charController;
+
+    // Use this for initialization
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * Speed;
+        float deltaZ = Input.GetAxis("Vertical") * Speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, Speed);
+        movement.y = Gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        _charController.Move(movement);
+    }
+}
