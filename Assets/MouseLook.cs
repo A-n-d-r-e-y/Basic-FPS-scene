b59bdd64170d39d9bc54b2c03b9ec7bd5460925b
Andrey Morozov <morozov@client-line.com>
2015-12-06 18:55:44 +0200

    refactoring

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -20,6 +20,14 @@ public class MouseLook : MonoBehaviour
 
     private float _rotationX = 0;
 
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+
+        if (body != null)
+            body.freezeRotation = true;
+    }
+
     void Update()
     {
         if (axes == RotationAxes.MouseX)
@@ -39,12 +47,11 @@ public class MouseLook : MonoBehaviour
         }
         else {
             // both horizontal and vertical rotation here
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
 
             _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
             _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
 
-            float rotationY = transform.localEulerAngles.y;
+            float rotationY = transform.localEulerAngles.y + (Input.GetAxis("Mouse X") * sensitivityHor);
 
             transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
         }
